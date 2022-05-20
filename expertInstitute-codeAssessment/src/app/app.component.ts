1cf1c5b900b19bd081bffa6a77a8c7a81a34d330
Walker <courtney.walker@accenturefederal.com>
2022-05-20 12:07:20 -0500

    Final Push

--- expertInstitute-codeAssessment/src/app/app.component.ts
+++ expertInstitute-codeAssessment/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'expertInstitute-codeAssessment';
+  public selection: number = 0;
+  public myAssets: number = 0;
+  public result: number = 0;
+  
+  public calculateAssets(){
+    this.result = this.myAssets * this.selection;
+  };
 }
